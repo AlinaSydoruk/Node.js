0a06dd708f9d9f296e3a68c3a436c53e8c1782c8
AlinaSydoruk <128652015+AlinaSydoruk@users.noreply.github.com>
2023-05-30 22:30:10 +0200

    Add files via upload
    Lab5

---
+++ Node_Lab5/src/application/data/Users.ts
@@ -0,0 +1,7 @@
+import{IUser} from "../models/User";
+export const users : Array<IUser> = [
+    {id:"1A", name:"Bob",surname:"Milky", age:33,email:"bobmilky@gmail.com"},
+    {id:"2A", name:"Alan",surname:"Lipton", age:27,email:"alanlipton@gmail.com"},
+    {id:"3A", name:"Linda",surname:"Linda", age:25,email:"linda@gmail.com"},
+
+]
